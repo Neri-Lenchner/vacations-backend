984ae4932731e1522b735dfd306c410a8058dc88
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-03-29 17:19:32 +0300

    fixed fetching offset shit issues

--- src/controllers/vacation-controller.ts
+++ src/controllers/vacation-controller.ts
@@ -8,49 +8,41 @@ class VacationController {
     router = express.Router();
 
     constructor() {
-        this.router.post("/api/vacation", this.addVacation);
+        this.router.get("/api/vacation/", this.getVacationListOffset);
         this.router.get("/api/vacation-list/", this.getVacationList);
+        this.router.post("/api/vacation/", this.addVacation);
         this.router.put("/api/vacation/:id", this.updateVacation);
         this.router.delete("/api/vacation/:id", this.deleteVacation);
-        this.router.get("/api/vacation/", this.getVacationListOffset);
     }
 
-    public async addVacation(request: Request, response: Response) {
+    public async addVacation(request: Request, response: Response): Promise<void> {
         const vacation: Vacation = new Vacation(request.body);
         const vacationFromDB = await vacationService.addVacation(vacation);
         response.status(StatusCode.Created).json(vacationFromDB);
     }
 
-    public async updateVacation(request: Request, response: Response) {
+    public async updateVacation(request: Request, response: Response): Promise<void> {
         const id = +request.params.id;
         const vacation: Vacation = new Vacation(request.body);
         const vacationFromDB = await vacationService.updateVacation(id, vacation);
         response.json(vacationFromDB);
     }
 
-    public async deleteVacation(request: Request, response: Response) {
-        const id = +request.params.id;
+    public async deleteVacation(request: Request, response: Response): Promise<void> {
+        const id: number = +request.params.id;
         await vacationService.deleteVacation(id);
         response.sendStatus(StatusCode.NoContent);
     }
 
-    public async getVacationList(request: Request, response: Response) {
+    public async getVacationList(request: Request, response: Response): Promise<void> {
         const vacationList: Vacation[] = await vacationService.getVacationList();
         response.json(vacationList);
     }
 
-    // public async getVacationListOffset(request: Request, response: Response) {
-    //     const offset: number = +request.params.offset;
-    //     const limit: number = +request.params.limit;
-    //     const vacationlistOffset: Vacation[] = await vacationService.getVacationListOffset(offset, limit);
-    //     response.json(vacationlistOffset);
-    // }
-
     public async getVacationListOffset(request: Request, response: Response) {
-        const offset = Number(request.query.offset) || 0;
-        const limit = Number(request.query.limit) || 10;
-
-        const vacationList = await vacationService.getVacationListOffset(limit, offset);
+        const limit: number = Number(request.query.limit) || 10;
+        const offset: number = Number(request.query.offset) || 0;
+        const vacationList: Vacation[] = await vacationService.getVacationListOffset(limit, offset);
         response.json(vacationList);
     }
 
