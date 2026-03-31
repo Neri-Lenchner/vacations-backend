ac1d8f31f02f6592b2df9ae9979691b794004b38
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-03-31 14:43:08 +0300

    added userController.ts

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import {loggerMiddleware} from "./middleware/logger-middleware";
 import {errorMiddleware} from "./middleware/error-middleware";
 import {authController} from "./controllers/auth-controller";
+import {userController} from "./controllers/user-controller";
 import {vacationController} from "./controllers/vacation-controller";
 
 
@@ -13,6 +14,7 @@ class App {
         server.use(express.json());
         server.use(loggerMiddleware.consoleLog);
         server.use(authController.router);
+        server.use(userController.router);
         server.use(vacationController.router);
         server.use(errorMiddleware.serverError);
         server.use(errorMiddleware.catchAll);
---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,36 @@
+import express, {Request, Response} from "express";
+import {userService} from "../services/user-service";
+import {User} from "../models/user.model";
+import {StatusCode} from "../models/enums";
+
+class UserController {
+
+    public router = express.Router();
+
+    constructor() {
+        this.router.get("/api/users/", this.getUserList);
+        this.router.put("/api/users/:id", this.updateUser);
+        this.router.delete("/api/users/:id", this.deleteUser);
+    }
+
+    public async getUserList(request: Request, response: Response) {
+        const userList: User[] = await userService.getUserList();
+        response.json(userList);
+    }
+
+    public async updateUser(request: Request, response: Response) {
+        const id = +request.params.id;
+        const user = new User(request.body);
+        await userService.updateUser(id, user);
+        response.json(user);
+    }
+
+    public async deleteUser(request: Request, response: Response) {
+        const id = +request.params.id;
+        await userService.deleteUser(id);
+        response.sendStatus(StatusCode.NoContent);
+    }
+
+}
+
+export const userController = new UserController();
