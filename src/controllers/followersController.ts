7f1c923ece67a4cae911c40402b5469cf3e574c1
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-03 19:19:39 +0300

    added followerController.addFollower

--- src/controllers/followersController.ts
+++ src/controllers/followersController.ts
@@ -1,17 +1,25 @@
 import express, {Request, Response} from "express";
 import {followersService} from "../services/followersService";
 import {Follower} from '../models/follower.model'
+import {StatusCode} from "../models/enums";
 
 class FollowersController {
     public router = express.Router();
 
     constructor() {
         this.router.get("/api/vacations/followers/", this.getFollowersList);
+        this.router.post("/api/vacations/followers", this.addFollower);
     }
 
     public async getFollowersList(request: Request, response: Response): Promise<void> {
         const followersList: Follower[] = await followersService.getFollowersList();
         response.json(followersList);
+    }
+
+    public async addFollower(request: Request, response: Response): Promise<void> {
+        const follower: Follower = new Follower(request.body);
+        const followerFromDB: Follower = await followersService.addFollower(follower);
+        response.status(StatusCode.Created).json(followerFromDB);
     }
 
 }
