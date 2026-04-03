75f65c35b534138137ea7e70129c0b28d4081602
Neri-Lenchner <n.l.y.a.lench@gmail.com>
2026-04-03 05:52:16 +0300

    modified:   src/services/uploadImageService.ts

--- src/services/uploadImageService.ts
+++ src/services/uploadImageService.ts
@@ -1,5 +1,25 @@
-class UpdateImageService {
+import multer from "multer";
 
+
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: (req, file, callback) => {
+        callback(null, "app" + Date.now());
+    }
+})
+
+class UpdateImageService {
+    public upload = multer({
+        storage,
+        fileFilter(req, file, callback) {
+            const allowed = ["image/jpeg", "image/png", "image/gif", "image/jpg"];
+            if (allowed.includes(file.mimetype)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Invalid file type"));
+            }
+        }
+    });
 }
 
 export const updateImageService = new UpdateImageService();
